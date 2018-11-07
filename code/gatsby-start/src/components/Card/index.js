16aa663185158e8c64611bf27073480dfe14740e
unknown <tjcold@hotmail.com>
2018-11-07 13:30:51 +0100

    test

--- code/gatsby-start/src/components/Card/index.js
+++ code/gatsby-start/src/components/Card/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./styles.module.css";
-
+//test
 export default({ children }) => (
     <div className = { styles.Card }>
             <div className = { styles.Photo }></div>
